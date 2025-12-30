0fcb0f303040b0686eb75652fe46b22265e19f42
amanmaheshwari01 <amanmah2929@gmail.com>
2025-12-30 15:04:33 +0530

    last

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ app.use('/api/blog', blogRouter)
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
-    console.log('Server is running on port ' + PORT)
+    console.log('Server is running on port: ' + PORT)
 })
 
 export default app;
